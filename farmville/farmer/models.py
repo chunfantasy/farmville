881bf37d4a7d7b78d874034cee2624e6cfaa5167
Bomullsdotten <sondre_erstad@hotmail.com>
2013-09-18 12:59:15 +0200

    Update models.py

--- farmville/farmer/models.py
+++ farmville/farmer/models.py
@@ -4,6 +4,6 @@ from django.contrib.admin
 # Create your models here.
 class Farmer(models.Model):
     name = CharField(max_length=50)
-    phone_number = IntField(max_length = 8) #### is IntField correct???
+    phone_number = IntegerField(max_length=8, min_length=8) #### is IntField correct???
     email = CharField(max_length=100)
     
