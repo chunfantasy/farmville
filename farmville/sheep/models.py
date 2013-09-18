d2dfcab3cb96989197da6f93e241103776908c81
Bomullsdotten <sondre_erstad@hotmail.com>
2013-09-18 13:04:07 +0200

    Update models.py

--- farmville/sheep/models.py
+++ farmville/sheep/models.py
@@ -6,7 +6,7 @@ class Sheep(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
     birth_place = models.CharField(max_length = 100)
-    birth_date = models.IntegerField(default =2013, min_length = 4)
+    birth_date = models.IntegerField(default =2013, min_length = 4) #is this alowed
     veigth = models.IntegerField(default = 0)
 
     def __unicode__(self):
