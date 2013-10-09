385687a7df9a0614a5a8b14b26a2aa60affcd2e6
chunfantasy <chunfantasy@hotmail.com>
2013-10-09 14:30:35 +0200

    worked on farmer registration

---
+++ farmville/farmer/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('farmville.farmer.views',
+    # Examples:
+    # url(r'^$', 'farmville.views.home', name='home'),
+    # url(r'^farmville/', include('farmville.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^farmerRegister$', 'farmerRegister'),
+    url(r'^farmerLogin$', 'farmerLogin'),
+    url(r'^farmerLoginSuccess$', 'farmerLoginSuccess'),
+)
