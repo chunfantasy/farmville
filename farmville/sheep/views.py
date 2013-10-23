cef387ab6138556fae64b5c922762af48dfabf5e
Impster <ivar.flakstad@gmail.com>
2013-10-23 13:38:24 +0200

    Real names

--- farmville/sheep/views.py
+++ farmville/sheep/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Create your views here.
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
@@ -6,9 +7,20 @@ from datetime import date
 from farmville.farmer.models import Farmer
 from farmville.sheep.models import Sheep
 from django.contrib.auth.models import AbstractUser
+import random
+import string
+
+
 
 
 def sheepGenerate(request):
+    common_names = ['Anne','Inger','Kari','Marit','Ingrid','Liv','Eva','Berit','Astrid',
+                'Bjørg','Hilde','Anna','Solveig','Marianne','Randi','Ida','Nina',
+                'Maria','Elisabeth','Kristin','Bente','Heidi','Silje','Hanne',
+                'Jan','Per','Bjørn','Ole','Lars','Kjell','Knut','Arne','Svein',
+                'Thomas','Hans','Geir','Tor','Morten','Terje','Odd','Erik','Martin',
+                'Andreas','John','Anders','Rune','Trond','Tore','Daniel','Jon']
+
     farmer = request.user
     s = Sheep.objects.all()
     s.delete()
@@ -16,8 +28,8 @@ def sheepGenerate(request):
     for i in range(50):
         sheep = Sheep()
         sheep.Farmer = farmer
-        sheep.name = str(i)
-	sheep.birthday = date.today()
+        sheep.name = common_names.pop(random.randint(0,50-i-1))
+        sheep.birthday = date.today()
         sheep.sheepid = farmer.farmerid + str(sheep.birthday)[3] + str(i+1).zfill(4)
         sheep.save()
         print(sheep.name,sheep.birthday,sheep.sheepid)
