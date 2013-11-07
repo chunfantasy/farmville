941732aa762860c1d7f0afca60fdcda18bed1d56
Bomullsdotten <sondre_erstad@hotmail.com>
2013-11-07 15:22:59 +0100

    Update views.py

--- farmville/sheep/views.py
+++ farmville/sheep/views.py
@@ -16,6 +16,20 @@ common_names = ['Anne','Inger','Kari','Marit','Ingrid','Liv','Eva','Berit','Astr
                 'Jan','Per','Bjorn','Ole','Lars','Kjell','Knut','Arne','Svein',
                 'Thomas','Hans','Geir','Tor','Morten','Terje','Odd','Erik','Martin',
                 'Andreas','John','Anders','Rune','Trond','Tore','Daniel','Jon']
+               
+def sendMail(request):
+    subject = "Sau under angrep"
+    message = "test"
+    broadcaster = "sondre.erstad@gmail.com"
+    receiver = ['sondre_erstad@hotmail.com']
+    if receiver and broadcaster and message:
+        try:
+            send_mail(subject,message, broadcaster, receiver)
+        except BadHeaderError:
+            return "Invalid header found"
+        return HttpResponseRedirect('/farmer/farmerLogin')
+    else:
+        return "make sure all fields are valid!"
 
 def sheepGenerateTest(request):
     names = common_names[::]
