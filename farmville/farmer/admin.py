cabf07f31275b29746dc824e115b987abd492ff6
Impster <ivar.flakstad@gmail.com>
2013-11-08 09:51:26 +0100

    fixed github errors

--- farmville/farmer/admin.py
+++ farmville/farmer/admin.py
@@ -5,16 +5,12 @@ class FarmerAdmin(admin.ModelAdmin):
 
     def queryset(self, request):
         qs = super(FarmerAdmin, self).queryset(request)
-<<<<<<< HEAD
         if request.user.is_superuser:
             self.exclude = {}
             return qs
-=======
-
 	if request.user.is_superuser:
             self.exclude = {}
 	    return qs
->>>>>>> 0157500cbe17f6374fbf24e2ac809f8665312210
         self.exclude = ['username', 'user_permissions', 'groups', 'last_login', 'password', 'staff', 'farmerId', 'date_joined', 'is_staff', 'is_superuser', 'is_active']
         return qs.filter( username = request.user.username)
     list_display = ('username', 'farmerId', 'first_name')
