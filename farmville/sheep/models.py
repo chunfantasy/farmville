0517f26535ae17d54e592c45c62a57f880ac3e93
Impster <ivar.flakstad@gmail.com>
2013-11-08 13:18:57 +0100

    SHEEEEEP3

--- farmville/sheep/models.py
+++ farmville/sheep/models.py
@@ -10,8 +10,12 @@ class Sheep(models.Model):
         if valSize  < 5:
             #if
             raise ValidationError(u'%s is not a valid ID, an ID' % value)
-    def validate_unique(self, exclude=None):
-        print "poop"
+
+    def validate_unique(value, exclude=None):
+        s = Sheep.objects.filter(farmer = Farmer)
+        for i in s:
+            if s.id == value:
+                raise ValidationError(u'%s is not a unique ID' % value)
 
     STATUS_CHOICES=(
 	(0,"Normal"),
@@ -22,7 +26,7 @@ class Sheep(models.Model):
     birthday = models.DateField(auto_now_add=False,null=True)
     birthplace = models.CharField(max_length=15, null=True)
     farmer = models.ForeignKey(Farmer, null=True)
-    sheepId = models.IntegerField(validators=[validate_size, validate_unique],max_length=12)
+    sheepId = models.CharField(validators=[validate_size, validate_unique],max_length=12)
     weight = models.FloatField(null=True, blank=True)
     status = models.IntegerField(max_length=1, choices=STATUS_CHOICES,default=0, null=True)
     latitude = models.FloatField(blank=True, null=True)
