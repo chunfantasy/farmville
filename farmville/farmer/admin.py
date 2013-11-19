90a4a5aeef0781a96142d23e416f4a6f70632fa9
vetleari <vetleari@stud.ntnu.no>
2013-11-19 19:04:49 +0100

    forandre farmerId

--- farmville/farmer/admin.py
+++ farmville/farmer/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from farmville.farmer.models import Farmer
+from farmville.sheep.models import Sheep
 
 class FarmerAdmin(admin.ModelAdmin):
     list_display = ('username', 'farmerId', 'first_name')
@@ -9,7 +10,14 @@ class FarmerAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             self.exclude = {}
             return qs
-        self.exclude = ['username', 'user_permissions', 'groups', 'last_login', 'password', 'staff', 'farmerId', 'date_joined', 'is_staff', 'is_superuser', 'is_active']
+        self.exclude = ['username', 'user_permissions', 'groups', 'last_login', 'password', 'staff', 'date_joined', 'is_staff', 'is_superuser', 'is_active']
         return qs.filter( username = request.user.username)
+    def save_model(self, request, obj, form, change):
+        if not(obj.farmerId == request.user.farmerId):
+            liste = obj.own.all()
+            for s in liste:
+                s.sheepId = str(obj.farmerId) + s.sheepId[7:12]
+                s.save()
+        obj.save()
 
 admin.site.register(Farmer, FarmerAdmin)
